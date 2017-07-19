07cee443a11d6ada21ae844dfc896382b0fa0304
yoghourt66 <yoghourt66@163.com>
2017-07-19 23:17:33 +0800

    add mini detail for herodetail

--- heroManage05/src/app/heroes.component.ts
+++ heroManage05/src/app/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 import { Hero } from './hero';
 import {HeroService} from './hero.service';
@@ -7,69 +8,8 @@ import {HeroService} from './hero.service';
 
 @Component({
   selector: 'my-heroes',
-  template: `
-    <h1>{{title}}</h1>
-    
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-        <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero===selectedHero">
-            <!--each hero goes here-->
-            <span class="badge">{{hero.id}}</span>{{hero.name}}
-        </li>
-    </ul>
-    <hero-detail [hero]="selectedHero"><hero-detail>
-    <!--父组件通过把它的selectedHero属性绑定到子组件的hero属性上，告诉子组件显示哪个英雄，hero为绑定目标-->
-  `,
-  styles:[`
-    .selected{
-        background-color:#CFD8DC !important;
-        color:white;
-    }
-    .heroes{
-        margin:0 0 2em 0;
-        list-style-type:none;
-        padding:0;
-        width:15em;
-    }
-    .heroes li{
-        cursor:pointer;
-        position:relative;
-        left:0;
-        background-color:#EEE;
-        margin:.5em;
-        padding:.3em 0;
-        height:1.6em;
-        border-radius:4px;
-    }
-    .heroes li .selected:hover{
-        backgroung-color:#BBD8DC !important;
-        color:white;
-    }
-    .heroes li:hover{
-        color:#607D8B;
-        background-color:#DDD;
-        left:1em;
-    }
-    .heroes .text{
-        position:relative;
-        top:-3px;
-    }
-    .heroes .badge{
-        display:inline-block;
-        font-size:small;
-        color:white;
-        padding:0.8em 0.7em 0 0.7em;
-        background-color:#607D8B;
-        line-height:1em;
-        position:relative;
-        left:-1px;
-        top:-4px;
-        height:1.8em;
-        margin-right:0.8em;
-        border-radius:4px 0 0 4px;
-    }
-  `],
-
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
 })
 
 export class HeroesComponent implements OnInit{
@@ -90,7 +30,10 @@ export class HeroesComponent implements OnInit{
   
   heroes:Hero[];
   
-  constructor(private heroService:HeroService){
+  constructor(
+    private heroService:HeroService,
+    private router: Router
+  ){
     //this.heroes = this.heroService.getHeroes();
     //构造函数获取数据的操作用生命周期钩子代替。
   }
@@ -120,6 +63,11 @@ export class HeroesComponent implements OnInit{
     //此时页面中不需要heroes数据的地方先渲染，2s后，需要用heroes数据渲染的视图后渲染出来。不必等所有数据全部加载完成后整体渲染。
   }
 
+
+  gotoDetail():void{
+    this.router.navigate(['/detail',this.selectedHero.id]);
+    //使用链接参数数组传递到路由的navigate()，与在模板中绑定[routerLink]一样。
+  }
   
 }
 
